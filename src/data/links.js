140155b87cc53ceae3cb3281c834903965722ec8
lee-charles-fecc <lee.charles@fecc.coop>
2025-11-14 16:41:57 -0600

    ._.

--- src/data/links.js
+++ src/data/links.js
@@ -2,85 +2,85 @@ export const INTRANET_LINKS = [
     {
         id: 1,
         name: "AirFlow Test",
-        url: "https://first2etltest/",
-        icon: "https://first2etltest/static/pin_32.png",
+        url: `${process.env.REACT_APP_AIRFLOW_TEST}/test`,
+        icon: `${process.env.REACT_APP_AIRFLOW_TEST}/static/pin_32.png`,
         description: "Workflow Management"
     },
     {
         id: 2,
         name: "AirFlow",
-        url: "https://first2etl/",
-        icon: "https://first2etl/static/pin_32.png",
+        url: `${process.env.REACT_APP_AIRFLOW}`,
+        icon: `${process.env.REACT_APP_AIRFLOW}/static/pin_32.png`,
         description: "Workflow Management"
     },
     {
         id: 3,
         name: "Docker Registry",
-        url: "https://sczpm01:5000/",
-        icon: "https://sczpm01:5000/images/favicons/favicon-32x32.png",
+        url: `${process.env.REACT_APP_DOCKER_REGISTRY}`,
+        icon: `${process.env.REACT_APP_DOCKER_REGISTRY}/images/favicons/favicon-32x32.png`,
         description: "Container Registry"
     },
     {
         id: 4,
         name: "Grafana",
-        url: "http://first2monitor:3000/",
-        icon: "http://first2monitor:3000/public/img/fav32.png",
+        url: `${process.env.REACT_APP_GRAFANA}`,
+        icon: `${process.env.REACT_APP_GRAFANA}/public/img/fav32.png`,
         description: "Monitoring & Analytics"
     },
     {
         id: 5,
         name: "First2Data",
-        url: "https://first2data/",
-        icon: "https://first2data/favicon.ico",
+        url: `${process.env.REACT_APP_FIRST2DATA}`,
+        icon: `${process.env.REACT_APP_FIRST2DATA}/favicon.ico`,
         description: "Production Environment"
     },
     {
         id: 6,
         name: "First2Data Test",
-        url: "https://first2datatest/",
-        icon: "https://first2datatest/favicon.ico",
+        url: `${process.env.REACT_APP_FIRST2DATA_TEST}`,
+        icon: `${process.env.REACT_APP_FIRST2DATA_TEST}/favicon.ico`,
         description: "Testing Environment"
     },
     {
         id: 7,
         name: "FECC Sharepoint",
-        url: "https://firstelectric.sharepoint.com/",
-        icon: "https://firstelectric.sharepoint.com/_layouts/15/images/favicon.ico?rev=43",
+        url: `${process.env.REACT_APP_SHAREPOINT}`,
+        icon: `${process.env.REACT_APP_SHAREPOINT}/_layouts/15/images/favicon.ico?rev=43`,
         description: "FECC Home Screen"
     },
     {
         id: 8,
         name: "Open Project",
-        url: "https://first2project/",
-        icon: "https://first2project/custom_style/1724177103/favicon/favicon.ico",
+        url: `${process.env.REACT_APP_FIRST2PROJECT}`,
+        icon: `${process.env.REACT_APP_FIRST2PROJECT}/custom_style/1724177103/favicon/favicon.ico`,
         description: "FECC Project Management"
     },
     {
         id: 9,
         name: "iVue",
-        url: "https://firstelectric.cloud.coop/connect-financials/#",
-        icon: "https://firstelectric.cloud.coop/connect-financials/favicon.ico",
+        url: `${process.env.REACT_APP_IVUE}/#`,
+        icon: `${process.env.REACT_APP_IVUE}/favicon.ico`,
         description: "FECC Time Keeper"
     },
     {
         id: 10,
         name: "Help Desk",
-        url: "https://itservice.fec.local/HomePage.do",
-        icon: "https://itservice.fec.local/images/favicon.ico?14600",
+        url: `${process.env.REACT_APP_HELP_DESK}/HomePage.do`,
+        icon: `${process.env.REACT_APP_HELP_DESK}/images/favicon.ico?14600`,
         description: "FECC Help Desk"
     },
     {
         id: 11,
         name: "Virus Total",
-        url: "https://www.virustotal.com/gui/home/upload",
-        icon: "https://virustotal.com/gui/images/favicon.svg",
+        url: `${process.env.REACT_APP_VIRUS_TOTAL}/home/upload`,
+        icon: `${process.env.REACT_APP_VIRUS_TOTAL}/images/favicon.svg`,
         description: "Virus File Scanner"
     },
     {
         id: 12,
         name: "Mimecast",
-        url: "https://www.virustotal.com/gui/home/upload",
-        icon: "https://login-us.mimecast.com/u/assets/images/mpp_favicon.ico",
+        url: `${process.env.REACT_APP_MIMECAST}/login/?gta=portal#/login`,
+        icon: `${process.env.REACT_APP_MIMECAST}/assets/images/mpp_favicon.ico`,
         description: "Mimecast Email Spam Box"
     }
 ];
