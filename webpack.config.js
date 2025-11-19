bdb6509c30a513d520135a46a7c78675195d64b4
lee-charles-fecc <lee.charles@fecc.coop>
2025-11-19 09:43:10 -0600

    Abstracted to using express as a backend.
    This should prevent any username/password data from being viewed in plain text within the source via secrets.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,18 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: [
-        'webpack-dev-server/client?http://localhost:8080', // Adjust port as needed
-        'webpack/hot/only-dev-server',
-        './src/index.js' // Your application's entry point
-    ],
+    entry: './src/index.js', // Your application's entry point
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/'
     },
-    devServer: {
-        hot: true, // Enable HMR
-        static: {
-            directory: path.join(__dirname, 'public'), // Serve static files from 'public'
-        },
-        port: 8080, // Adjust port as needed
-        historyApiFallback: true, // For client-side routing
-    },
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html"),
+        }),
     ],
     module: {
         rules: [
